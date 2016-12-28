f53c32a4c8fc035aa20ace98ddeb1c38e4a97cd9
bbenichou <oukabi2010>
2016-12-28 17:20:12 +0000

    add createlogfile function to base_page

--- BakerRoss/TestSuite/TestSuite_ALL.py
+++ BakerRoss/TestSuite/TestSuite_ALL.py
@@ -1,6 +1,10 @@
 #Import tests
 from Tests.loginTest import *
 from Tests.ProdPageTest import *
+from TestsConfiguration import HTMLTestRunner
+from io import StringIO
+import io
+import six
 
 #define testloader variable for each tests
 test1 = unittest.TestLoader().loadTestsFromTestCase(login)
@@ -10,7 +14,17 @@ test2 = unittest.TestLoader().loadTestsFromTestCase(productpage)
 all_tests = unittest.TestSuite([test1,test2])
 
 #run the suite
-unittest.TextTestRunner(verbosity=2).run(all_tests)
+#unittest.TextTestRunner(verbosity=2).run(all_tests)
+
+outfile = open("C:\Report.html", "w")
+
+runner = HTMLTestRunner.HTMLTestRunner(
+                stream=outfile,
+                title='Suite Report:  ALL test',
+                description='Report Output.'
+                )
+
+runner.run(all_tests)
 
 #close the browser
 driver.quit()
